ab51c2e3c4ded315d726ddde99fe2799d9cbe6b8
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-07-29 07:37:27 +0530

    Update Two Sum.py

--- Array/medium/Two Sum.py
+++ Array/medium/Two Sum.py
@@ -11,3 +11,26 @@ https://leetcode.com/problems/two-sum/
             if rem in map:
                 return [i , map[rem]]
             map[nums[i]] = i
+
+...........................................................
+#used greedy approach , and sorting
+      k = list(nums)
+        k.sort()
+        ans = []
+        i=0
+        j=len(nums)-1
+
+        while i < j:
+            sum = k[i]+k[j]
+            if sum == target:
+                ans =[k[i], k[j]]
+                break
+            elif sum < target:
+                i+=1
+            elif sum > target:
+                j-=1
+
+        ans[0] = nums.index(ans[0])
+        nums[ans[0]] = -1 #added to modify the index, so that if an duplicate value there it wont affect result
+        ans[1] = nums.index(ans[1])
+        return ans
