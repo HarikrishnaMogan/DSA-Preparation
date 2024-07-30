6108629648db6db99c5be2438ed03d450cde1d79
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-07-30 07:33:58 +0530

    Create Kadane's Algorithm, maximum subarray sum.py

---
+++ Algorithms/Kadane's Algorithm, maximum subarray sum.py
@@ -0,0 +1,17 @@
+
+
+ def maxSubArray(self, nums):
+       
+       # this is kadanes algoritm
+       # according to it if a sum is less than zero , make the sum as zero, since we only want maximun sum
+       
+
+        maxi = nums[0]
+        sum = 0
+        for i in nums:
+            sum += i
+            maxi = max(maxi, sum)
+            if sum < 0: # make sum zero, if sum is negative
+                sum =0
+        return maxi
+            
