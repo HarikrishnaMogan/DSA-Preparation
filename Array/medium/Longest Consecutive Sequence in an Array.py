671f3cc13527fbd6a681f88f457b396d8f49a271
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-01 07:24:16 +0530

    Update Longest Consecutive Sequence in an Array.py

--- Array/medium/Longest Consecutive Sequence in an Array.py
+++ Array/medium/Longest Consecutive Sequence in an Array.py
@@ -1,6 +1,7 @@
 
 https://takeuforward.org/data-structure/longest-consecutive-sequence-in-an-array/
 #Better approch
+TC - Nlog(N), SC - O(1)
 
 def longConsSeq(arr):
     if len(arr) == 0:
@@ -22,3 +23,31 @@ def longConsSeq(arr):
     return long
 
 print(longConsSeq([100, 200, 1, 3, 2, 4, 1,1,2,2,101]))
+
+................................................................
+
+#optimal approch
+TC - O(N), SC- O(N)
+def longConsSeq(arr):
+    if len(arr) == 0:
+        return 0
+    arrSet = set() #useds set to filter out unique elements
+    for i in arr:
+        arrSet.add(i)
+    count = 0
+    long = 1
+    for i in arrSet:
+        x = i
+        count = 0
+        if x-1 not in arrSet: #checked if x-1 is present if not looping through to count the sequence
+            while x in arrSet:
+                count+=1
+                x+=1
+            long = max(long, count)
+    return long
+            
+
+
+print(longConsSeq([100, 200, 1, 3, 2, 4, 1,1,2,2,101]))
+
+
