84dcd6b4e30a05cc7773ad5235de4f01096d339a
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-07 08:33:55 +0530

    Update 4 sum.py

--- Array/Hard/4 sum.py
+++ Array/Hard/4 sum.py
@@ -3,10 +3,10 @@ https://leetcode.com/problems/4sum/
    nums.sort()
         ans = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:#need to avoid duplicate values
                 continue
             for j in range(i+1,len(nums)):
-                if j != i+1 and nums[j]== nums[j-1]:
+                if j != i+1 and nums[j]== nums[j-1]:#need to avoid duplicate values
                     continue
                 k = j+1
                 l= len(nums)-1
@@ -23,8 +23,8 @@ https://leetcode.com/problems/4sum/
                         k+=1
                         l-=1
 
-                        while k < l and nums[k] == nums[k-1]:
+                        while k < l and nums[k] == nums[k-1]: #need to avoid duplicate values
                             k+=1
-                        while k < l and nums[l]==nums[l+1]:
+                        while k < l and nums[l]==nums[l+1]:#need to avoid duplicate values
                             l-=1
         return ans 
