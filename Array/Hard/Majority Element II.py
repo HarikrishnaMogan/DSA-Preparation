d308ec1518b092d67b132336c64f330b9f198219
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-07 07:42:44 +0530

    Update Majority Element II.py

--- Array/Hard/Majority Element II.py
+++ Array/Hard/Majority Element II.py
@@ -18,4 +18,41 @@ https://leetcode.com/problems/majority-element-ii/
         if count > majority:
             ans.append(ele)
         return ans
+     #...........................................................
+
+#optimized solution, TC - O(N), SC- O(1)
+ # finding the most frequent element
+        count1 = 0
+        count2 = 0
+        ele1 = float('-inf')
+        ele2 = float('-inf')
+        for i in nums:
+            if count1 == 0 and ele2 != i:
+                ele1 = i
+                count1 =1
+            elif count2 == 0 and ele1 != i:
+                ele2 = i
+                count2 =1
+            elif ele1 == i:
+                count1+=1
+            elif ele2 == i:
+                count2+=1
+            else:
+                count1-=1
+                count2-=1
+        # rechecking and verifying the element actual count
+        count1 = 0
+        count2 = 0
+        ans = []
+        for i in nums:
+            if ele1 == i:
+                count1+=1
+            if ele2== i:
+                count2+=1
+        if count1 > len(nums)//3:
+            ans.append(ele1)
+        if count2 > len(nums)//3:
+            ans.append(ele2)
+        return ans
+            
         
