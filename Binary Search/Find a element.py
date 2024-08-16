bfc78a23f3669a273f9dedcafda2328ae7d78b70
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-16 08:14:42 +0530

    Update Find a element.py

--- Binary Search/Find a element.py
+++ Binary Search/Find a element.py
@@ -11,3 +11,22 @@ https://leetcode.com/problems/binary-search/
             else:
                 h = mid-1
         return -1
+...............................................
+ def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def binarySearch(nums,l,h):
+            if l > h:
+                return -1
+
+            mid = (l+h)//2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                return binarySearch(nums,mid+1,h)
+            else:
+                return binarySearch(nums,l,mid-1)
+        return binarySearch(nums,0,len(nums)-1)
