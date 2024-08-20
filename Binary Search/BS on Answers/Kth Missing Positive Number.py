f1dbb8787350001ab2be59c812bb9af86b8f8397
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-20 10:34:59 +0530

    Update Kth Missing Positive Number.py

--- Binary Search/BS on Answers/Kth Missing Positive Number.py
+++ Binary Search/BS on Answers/Kth Missing Positive Number.py
@@ -7,3 +7,25 @@ https://leetcode.com/problems/kth-missing-positive-number/description/
             else:
                 break
         return k
+...............................................................
+
+#binary search, optimised approach
+def findKthPositive(self, arr: List[int], k: int) -> int:
+        low=0
+        high=len(arr)-1
+        while low<=high:
+            mid=(low+high)//2
+            missingNumbers = arr[mid]-(mid+1) #please refer striver video if have doubt
+            if missingNumbers <k:
+                low=mid+1
+            else:
+                high=mid-1
+        
+        #missing = arr[high] - (high+1), more = k-missing
+        #ans = arr[high]+more
+        #ans = arr[high]+k -(arr[high]-(high+1))
+        #ans = arr[high]+k-arr[high]+high+1 
+        #ans = k+high+1, if you observer high+1=low
+        #so ans = (high+1)+k|| ans=low+k
+
+        return low+k
