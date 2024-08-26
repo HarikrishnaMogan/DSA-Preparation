f6008ed684abd3da95aa5ebcf29ca0f251389bde
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-26 08:57:59 +0530

    Update Find Minimum in Rotated Sorted Array II.py

--- Binary Search/Find Minimum in Rotated Sorted Array II.py
+++ Binary Search/Find Minimum in Rotated Sorted Array II.py
@@ -21,4 +21,35 @@ https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
                     ans=nums[mid]
                 high=mid-1
         return ans
+
+.............................................
+#if index asked
+
+nums = [2,2,2,0,1]
+
+low=0
+high=len(nums)-1
+ans = float('inf')
+index=-1
+while low<=high:
+    mid=(low+high)//2
+    if nums[low] == nums[mid] and nums[mid]==nums[high]:
+        if nums[low] < ans:
+            ans=nums[low]
+            index=low
+        low+=1
+        high-=1
+        continue
+
+    if nums[low] <= nums[mid]:
+        if nums[low] < ans:
+            ans=nums[low]
+            index=low
+        low=mid+1
+    else:
+        if nums[mid] < ans:
+            ans=nums[mid]
+            index=low
+        high=mid-1
+print(ans,index)
         
