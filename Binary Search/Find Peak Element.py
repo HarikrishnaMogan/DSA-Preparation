09586f93695ba53d061fc9c090e397acc62b30ce
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-08-27 07:00:03 +0530

    Update Find Peak Element.py

--- Binary Search/Find Peak Element.py
+++ Binary Search/Find Peak Element.py
@@ -30,3 +30,32 @@ def findPeakElement(self, nums):
             #if both cases won't have we have multiple peaks and we can either go to right or left
             else:
                 h=m-1    
+...........................................................................
+
+#without edge cases outside and handling it inside
+
+ n = len(nums)
+
+        l = 0
+        h=n-1
+        while l<=h:
+            m=(l+h)//2
+            before= float('-inf')
+            after= float('-inf')
+            if m-1 >=0:
+                before = nums[m-1]
+            if m+1 < n:
+                after = nums[m+1]
+            if nums[m] > before and nums[m] > after:
+                return m
+            
+            #check the mid if we are in increasing or decreasing slope
+            #if increase slope, peak is on the right
+            if nums[m] > before:
+                l=m+1
+            #if decrease slope, peak is on the left
+            elif nums[m] > after:
+                h=m-1    
+            #if both cases won't have we have multiple peaks and we can either go to right or left
+            else:
+                h=m-1    
