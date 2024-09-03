476c3f442a2b07a02c7a0fcf9960c37a7e70a29b
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-03 09:28:55 +0530

    Update Longest Common Prefix.py

--- Strings/Easy/Longest Common Prefix.py
+++ Strings/Easy/Longest Common Prefix.py
@@ -9,3 +9,16 @@ class Solution:
                     return res
             res += strs[0][i]
         return res
+..........................................
+#optimised , O(nlogn)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort()
+        first = strs[0]
+        last=strs[-1]
+        res=""
+        for i in range(min(len(first),len(last))):
+            if first[i] != last[i]:
+                return res
+            res+=first[i]
+        return res
