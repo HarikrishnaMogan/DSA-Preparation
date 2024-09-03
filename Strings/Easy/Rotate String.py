0df8da76ccc452197e9d59c9d8adf6ba3f8324fd
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-03 08:38:00 +0530

    Create Rotate String.py

---
+++ Strings/Easy/Rotate String.py
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/rotate-string/description/
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        elif s==goal:
+            return True
+        last = len(goal)-1
+        for i in range(len(goal)):
+            goal = goal[last:]+goal[:last]
+            if s==goal:
+                return True
+        return False
