fdd57732b64b6d9cbc16a00a7e05ff8f44a3f7c7
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-12 07:49:15 +0530

    Create Search in Linked List.py

---
+++ LinkedList/Learn 1D LinkedList/Search in Linked List.py
@@ -0,0 +1,23 @@
+https://www.geeksforgeeks.org/problems/search-in-linked-list-1664434326/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=search-in-linked-list-1664434326
+
+#User function Template for python3
+
+
+''' Node of a linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def searchKey(self, n, head, key):
+        #Code here
+        temp = head
+        while temp:
+            if temp.data == key:
+                return True
+            temp = temp.next
+        return False
+
+
+
