6503cdedb8793d07d55a4c177f73d28646fcf830
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-16 10:07:42 +0530

    Update Reverse Linked List.py

--- LinkedList/Medium problems of LL/Reverse Linked List.py
+++ LinkedList/Medium problems of LL/Reverse Linked List.py
@@ -17,3 +17,24 @@ class Solution:
             prev = current
             current = next
         return prev
+
+#..........................................................
+#recurive solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head):
+            if head== None or head.next == None:
+                return head
+            
+            newHead = reverse(head.next)
+            front = head.next
+            front.next = head
+            head.next = None
+            return newHead
+        return reverse(head)
