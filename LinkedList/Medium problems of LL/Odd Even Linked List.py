76cb7708caef94a836b2a707a4ef5f34a1fcdd26
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-18 09:16:42 +0530

    Update Odd Even Linked List.py

--- LinkedList/Medium problems of LL/Odd Even Linked List.py
+++ LinkedList/Medium problems of LL/Odd Even Linked List.py
@@ -26,5 +26,31 @@ class Solution:
         else:
             temp.next = evenHead
         return head
+#......................................................
+#strivers solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return head
+        
+        odd = head
+        even = head.next
+        evenHead = head.next
+        while even != None and even.next != None:
+            odd.next = odd.next.next
+            even.next = even.next.next
+
+            odd = odd.next
+            even = even.next
+        
+        #attach the odd tail to evenHead
+        odd.next = evenHead
+
+        return head
 
         
