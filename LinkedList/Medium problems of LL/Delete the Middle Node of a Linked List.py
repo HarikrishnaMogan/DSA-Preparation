e8257569c14b1c0fb01ff656da18f4d8ce7996f7
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-09-19 08:27:13 +0530

    Update Delete the Middle Node of a Linked List.py

--- LinkedList/Medium problems of LL/Delete the Middle Node of a Linked List.py
+++ LinkedList/Medium problems of LL/Delete the Middle Node of a Linked List.py
@@ -20,3 +20,25 @@ class Solution:
         prev.next = prev.next.next
         slow.next = None
         return head
+#................................................
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return None
+        
+        slow = head
+        fast = head.next.next if head.next else None
+    
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        delNode = slow.next
+        slow.next = slow.next.next
+        delNode.next = None
+        return head
