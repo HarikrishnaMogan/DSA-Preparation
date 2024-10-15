81a4572889bc32ba429ce8b3e5e83443f64e9af0
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-10-15 08:04:25 +0530

    Update Count Good Numbers.py

--- Recurssion (strongHold)/Get a strong Hold/Count Good Numbers.py
+++ Recurssion (strongHold)/Get a strong Hold/Count Good Numbers.py
@@ -19,4 +19,25 @@ class Solution:
 
         even = n//2+n%2
         odd = n//2
+        return (power(5,even,mod)*power(4,odd,mod))%mod 
+
+#.............................................................................
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod = int(10**9+7)
+        def power(x,n,mod):
+            ans = 1
+            while n > 0:
+                if n%2 == 0:
+                    x = (x*x)%mod
+                    n=n//2
+                else:
+                    ans = ans*x
+                    ans%=mod 
+                    n=n-1
+            return ans
+                
+
+        even = n//2+n%2
+        odd = n//2
         return (power(5,even,mod)*power(4,odd,mod))%mod     
