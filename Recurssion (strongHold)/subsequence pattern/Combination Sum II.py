e2ae12a6343252b33d50f879588f013f60479a11
HarikrishnaMogan <86546230+HarikrishnaMogan@users.noreply.github.com>
2024-10-21 09:10:32 +0530

    Create Combination Sum II.py

---
+++ Recurssion (strongHold)/subsequence pattern/Combination Sum II.py
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/combination-sum-ii/description/
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        def combin(i,target,arr):
+            if target == 0:
+                ans.append(ds[:])
+                return
+            
+            for ind in range(i,len(arr)):
+                if ind > i and arr[ind] == arr[ind-1]:
+                    continue;
+                if arr[ind] > target:
+                    break;
+                
+                ds.append(arr[ind])
+                combin(ind+1,target-arr[ind],arr)
+                ds.pop()
+        ans = []
+        ds= []
+        candidates.sort()
+        combin(0,target,candidates)
+        return ans
